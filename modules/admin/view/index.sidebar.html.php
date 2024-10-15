d3a88ce3e105d99ecb1169749ad512e8277625be
GilmerFranco <gilmerfranko@hotmail.com>
2024-10-14 22:21:24 -0400

    Se ha mejorado el diseño y estructura de la barra lateral

--- modules/admin/view/index.sidebar.html.php
+++ modules/admin/view/index.sidebar.html.php
@@ -22,6 +22,7 @@
       <a class="collapsible-header white-text waves-effect waves-blue "><i class="material-icons white-text">settings_applications</i>Admin <i class="material-icons right white-text" style="margin-right:0;">arrow_drop_down</i></a>
       <div class="collapsible-body z-depth-1">
         <ul>
+          <li><a href="#" class="waves-effect waves-blue grey-text">Sistema</a></li>
           <li <?php if ($sSection == 'configuration')
               {
                 echo ' class="active"';
@@ -58,6 +59,36 @@
               Contactos
             </a>
           </li>
+          <li><a href="#" class="waves-effect waves-blue grey-text">Anuncios</a></li>
+          <li <?php if ($sSection == 'threads.views')
+              {
+                echo ' class="active"';
+              } ?>>
+            <a class="waves-effect waves-blue" href="<?php echo $extra->generateUrl('admin', 'threads'); ?>">
+              <i class="material-icons">loyalty</i>
+              Anuncios
+            </a>
+          </li>
+
+          <li <?php if ($sSection == 'contacts.views')
+              {
+                echo ' class="active"';
+              } ?>>
+            <a class="waves-effect waves-blue" href="<?php echo $extra->generateUrl('admin', 'contacts.views'); ?>">
+              <i class="material-icons">receipt</i>
+              Contáctos
+            </a>
+          </li>
+
+          <li <?php if ($sSection == 'views.locations')
+              {
+                echo ' class="active"';
+              } ?>>
+            <a class="waves-effect waves-blue" href="<?php echo $extra->generateUrl('admin', 'views.locations'); ?>">
+              <i class="material-icons">location_on</i>
+              Ubicaciones
+            </a>
+          </li>
           <li <?php if ($sSection == 'reports')
               {
                 echo ' class="active"';
@@ -67,49 +98,14 @@
               Reportes
             </a>
           </li>
-          <hr>
-          <li class="">
-            <ul class="collapsible collapsible-accordion">
-              <li <?php if ($sSection == 'contacts.view')
-                  {
-                    echo ' class="active"';
-                  } ?>>
-                <a class="collapsible-header white-text waves-effect waves-blue "><i class="material-icons white-text">description</i>Secciones<i class="material-icons right white-text" style="margin-right:0;">arrow_drop_down</i></a>
-                <div class="collapsible-body z-depth-1">
-                  <ul>
-                    <li <?php if ($sSection == 'contacts.view')
-                        {
-                          echo ' class="active"';
-                        } ?>>
-                      <a class="waves-effect waves-blue" href="<?php echo $extra->generateUrl('admin', 'contacts.views'); ?>">
-                        <i class="material-icons">description</i>
-                        Contáctos
-                      </a>
-                    </li>
-                  </ul>
-                  <ul>
-                    <li <?php if ($sSection == 'views.locations')
-                        {
-                          echo ' class="active"';
-                        } ?>>
-                      <a class="waves-effect waves-blue" href="<?php echo $extra->generateUrl('admin', 'views.locations'); ?>">
-                        <i class="material-icons">description</i>
-                        Ubicaciones
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-            </ul>
-          </li>
-          <hr>
+          <li><a href="#" class="waves-effect waves-blue grey-text">Transacciones</a></li>
           <li <?php if ($sSection == 'transactions')
               {
                 echo ' class="active"';
               } ?>>
             <a class="waves-effect waves-blue" href="<?php echo $extra->generateUrl('admin', 'transactions'); ?>">
               <i class="material-icons">wallet</i>
-              Transacciones
+              Movimientos
             </a>
           </li>
         </ul>
